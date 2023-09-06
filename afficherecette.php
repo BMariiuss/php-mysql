08295b1951549c6bab1a1b8a7a0e7dda7a9d7ecd
Mario <marius.bonnafous@gmail.com>
2023-09-06 11:23:37 +0200

    Les tableaux associatifs

--- afficherecette.php
+++ afficherecette.php
@@ -1,22 +1,53 @@
-<?php
-//tableau des recettes
-$recipes = [
-['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-['Couscous','[...]','mickael.andrieu@exemple.com',false,],
-];
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Affichage des recettes</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<h1>Affichage des recettes</h1>
+
 <ul>
-<?php for ($lines = 0; $lines <= 1; $lines++): ?>
-<li>
-<?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-</li>
-<?php endfor; ?>
+<?php
+//tableau des recettes
+$recipes = [
+    [
+    'title' => 'Cassoulet',
+    'recipe' => 'Etape1: des flageolets',
+    'author' => 'mickael.andrieu@exemple.com',
+    'is_enabled' => true,
+    ],
+    [
+    'title' => 'Couscous',
+    'recipe' => '',
+    'author' => 'mickael.andrieu@exemple.com',
+    'is_enabled' => false,
+    ],
+    [
+    'title' => 'Escalope milanaise',
+    'recipe' => 'Etape1: prenez une belle escalope',
+    'author' => 'mathieu.nebra@exemple.com',
+    'is_enabled' => true,
+    ]
+];
+
+foreach($recipes as $recipe) {
+    if($recipe['is_enabled'] == true){ ?>
+        <h2>
+           <?php echo $recipe['title'] ?>
+        </h2>
+        <h3>
+            <?php echo $recipe['recipe'] ?>
+            <br>
+            <?php echo $recipe['author']?>
+        </h3>
+
+    <?php
+    }
+    
+}
+
+?>
 </ul>
 </body>
 </html>
